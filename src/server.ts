572ab44acaa32f459a0fb35473198ea0432356aa
Allan Scala <allanscala@gmail.com>
2023-08-01 14:18:28 -0300

    importado as rotas do password no server

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 //ROUTES
 app.use(require("./routes/user/user.route"));
 app.use(require("./routes/login/login.route"));
+app.use(require("./routes/password/password.route"));
 
 app.listen(port, () => {
     console.log(`Server is running in PORT: ${port}`)
